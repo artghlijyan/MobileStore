c1d691744f1966ab7b502ea7fcc78de05e2a4cef
Arthur Ghlijyan <deckuser@hotmail.com>
2020-05-07 19:10:03 +0400

    Add project files.

---
+++ MobileStore/DbRepo/MobileContext.cs
@@ -0,0 +1,17 @@
+﻿using AspNetMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetMvc.DbRepo
+{
+    public class MobileContext: DbContext
+    {
+
+        DbSet<Phone> Phones { get; set; }
+        DbSet<Order> Orders { get; set; }
+
+        public MobileContext(DbContextOptions<MobileContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ MobileStore/Mocks/DbInitializer.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MobileStore.Mocks
+{
+    public class DbInitializer : DbContext
+    {
+
+    }
+}
